f4459235ed921a59984915721dfc8cb6d6db75e1
Giang <kaylin.pham@contemi.com>
2022-06-23 08:44:53 +0700

    refactor

--- src/components/HistoryWidget/index.js
+++ src/components/HistoryWidget/index.js
@@ -7,17 +7,14 @@ import empty from "../../assets/images/empty.png";
 import "./style.scss";
 import transaction from "../../assets/images/transaction.png";
 
-import { getMyBookingHistory } from "../../services/me.service";
+import { getBookingHistory } from "../../services/me.service";
 import Pagination from "../Pagination";
 import { SUCCESS, FAILED, CANCELED } from "../../constants/bookingStatus";
-import { getMyMatch } from "../../services/me.service";
 import { EMPTY } from "../../constants/default";
 import { decrypt, encryptKey } from "../../helpers/crypto.helper";
 import { OWNER, USER } from "../../constants/roles";
-import { getBookingHistoryByOwner } from "../../services/owner.service";
 
 function HistoryWidget() {
-
   const ITEMS_PER_PAGE = 10;
   const [historyBookingModels, setHistoryBookingModels] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
@@ -33,35 +30,19 @@ function HistoryWidget() {
     const credential = localStorage.getItem(encryptKey("credential"));
     if (credential) {
       const role = decrypt(credential).role;
-      if (role === OWNER) {
-        getBookingHistoryByOwner({ page, itemsPerPage })
-          .then((res) => {
-            setHistoryBookingModels(res.data);
-            setMaxPage(
-              res.maxResult % ITEMS_PER_PAGE === 0 && res.maxResult !== 0
-                ? res.maxResult / ITEMS_PER_PAGE
-                : Math.floor(res.maxResult / ITEMS_PER_PAGE) + 1
-            );
-          })
-          .finally(() => {
-            setIsLoading(false);
-          });
-      } else if (role === USER) {
-        getMyBookingHistory({ page, itemsPerPage })
-          .then((res) => {
-            setHistoryBookingModels(res.data);
-            setMaxPage(
-              res.maxResult % ITEMS_PER_PAGE === 0 && res.maxResult !== 0
-                ? res.maxResult / ITEMS_PER_PAGE
-                : Math.floor(res.maxResult / ITEMS_PER_PAGE) + 1
-            );
-          })
-          .finally(() => {
-            setIsLoading(false);
-          });
-      }
+      getBookingHistory({ page, itemsPerPage }, role === USER ? "me" : "owners")
+        .then((res) => {
+          setHistoryBookingModels(res.data);
+          setMaxPage(
+            res.maxResult % ITEMS_PER_PAGE === 0 && res.maxResult !== 0
+              ? res.maxResult / ITEMS_PER_PAGE
+              : Math.floor(res.maxResult / ITEMS_PER_PAGE) + 1
+          );
+        })
+        .finally(() => {
+          setIsLoading(false);
+        });
     }
-
   };
   const onChangePage = (page) => {
     getHistoryBooking(page);
@@ -72,7 +53,7 @@ function HistoryWidget() {
     if (status === SUCCESS) return "green bold";
     else if (status === FAILED) return "red bold";
     else if (status === CANCELED) return "yellow bold";
-  }
+  };
 
   return (
     <div className="pt-5 mt-5 w-100 px-5">
@@ -80,66 +61,85 @@ function HistoryWidget() {
         <img src={transaction} alt="Transaction" className="width-60 pe-3" />
         Booking History
       </h4>
-      {
-        isLoading ? (
-          <div
-            className="w-100 d-flex justify-content-center align-items-center"
-            style={{ height: "50vh" }}
-          >
-            <div className="spinner-border" role="status">
-              <span className="sr-only">Loading...</span>
-            </div>
+      {isLoading ? (
+        <div
+          className="w-100 d-flex justify-content-center align-items-center"
+          style={{ height: "50vh" }}
+        >
+          <div className="spinner-border" role="status">
+            <span className="sr-only">Loading...</span>
           </div>
-        ) : !historyBookingModels.length ? (
-          <div className="w-100 pt-5 d-flex justify-content-center align-items-center flex-column h-300">
-            <img src={empty} style={{ width: 80 }} />
-            <p
-              className="text-center nodata-text"
-              style={{ fontSize: "0.9rem" }}
-            >
-              No result available
-            </p>
-          </div>
-        ) : (
-          <>
-            <table className="table">
-              <thead>
-                <tr>
-                  <th scope="col" style={{ width: "15%" }}>Yard</th>
-                  <th scope="col" style={{ width: "10%" }}>Sub-yard</th>
-                  <th scope="col" style={{ width: "5%" }}>Type</th>
-                  <th scope="col" style={{ width: "10%" }}>Address</th>
-                  <th scope="col" style={{ width: "8%" }}>Price</th>
-                  <th scope="col" style={{ width: "8%" }}>Status</th>
-                  <th scope="col" style={{ width: "15%" }}>Note</th>
-                  <th scope="col" style={{ width: "8%" }}>Created By</th>
-                  <th scope="col" style={{ width: "15%" }}>Created At</th>
-                </tr>
-              </thead>
-              <tbody>
-                <>
-                  {
-                    historyBookingModels.map(historyBooking => (
-                      <tr>
-                        <td>{historyBooking.bigYardName}</td>
-                        <td>{historyBooking.subYardName}</td>
-                        <td>{historyBooking.type}</td>
-                        <td className="text-truncate" title={historyBooking.address}>{historyBooking.address}</td>
-                        <td>{historyBooking.price} VND</td>
-                        <td className={getColor(historyBooking.bookingStatus)}
-                        >{historyBooking.bookingStatus}</td>
-                        <td className="text-truncate" title={historyBooking.note}>{historyBooking.note || "N/A"}</td>
-                        <td>{historyBooking.createdBy}</td>
-                        <td>{historyBooking.createdAt}</td>
-                      </tr>
-                    ))
-                  }
-                </>
-              </tbody>
-            </table>
-          </>
-        )
-      }
+        </div>
+      ) : !historyBookingModels.length ? (
+        <div className="w-100 pt-5 d-flex justify-content-center align-items-center flex-column h-300">
+          <img src={empty} style={{ width: 80 }} />
+          <p className="text-center nodata-text" style={{ fontSize: "0.9rem" }}>
+            No result available
+          </p>
+        </div>
+      ) : (
+        <>
+          <table className="table">
+            <thead>
+              <tr>
+                <th scope="col" style={{ width: "15%" }}>
+                  Yard
+                </th>
+                <th scope="col" style={{ width: "10%" }}>
+                  Sub-yard
+                </th>
+                <th scope="col" style={{ width: "5%" }}>
+                  Type
+                </th>
+                <th scope="col" style={{ width: "10%" }}>
+                  Address
+                </th>
+                <th scope="col" style={{ width: "8%" }}>
+                  Price
+                </th>
+                <th scope="col" style={{ width: "8%" }}>
+                  Status
+                </th>
+                <th scope="col" style={{ width: "15%" }}>
+                  Note
+                </th>
+                <th scope="col" style={{ width: "8%" }}>
+                  Created By
+                </th>
+                <th scope="col" style={{ width: "15%" }}>
+                  Created At
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              <>
+                {historyBookingModels.map((historyBooking) => (
+                  <tr>
+                    <td>{historyBooking.bigYardName}</td>
+                    <td>{historyBooking.subYardName}</td>
+                    <td>{historyBooking.type}</td>
+                    <td
+                      className="text-truncate"
+                      title={historyBooking.address}
+                    >
+                      {historyBooking.address}
+                    </td>
+                    <td>{historyBooking.price} VND</td>
+                    <td className={getColor(historyBooking.bookingStatus)}>
+                      {historyBooking.bookingStatus}
+                    </td>
+                    <td className="text-truncate" title={historyBooking.note}>
+                      {historyBooking.note || "N/A"}
+                    </td>
+                    <td>{historyBooking.createdBy}</td>
+                    <td>{historyBooking.createdAt}</td>
+                  </tr>
+                ))}
+              </>
+            </tbody>
+          </table>
+        </>
+      )}
       <Pagination maxPage={maxPage} onChangePage={onChangePage} />
       <ToastContainer />
     </div>
--- src/services/me.service.js
+++ src/services/me.service.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 import { SERVICE_URL } from "../constants/default";
+import { USER } from "../constants/roles";
 import { decrypt, encryptKey } from "../helpers/crypto.helper";
 
 export const getMyIncomingMatches = async (params) => {
@@ -15,10 +16,14 @@ export const getMyIncomingMatches = async (params) => {
   return response ? response.data : null;
 };
 
-export const getMyBookingHistory = async (params) => {
+export const getBookingHistory = async (params, role) => {
   const credential = localStorage.getItem(encryptKey("credential"));
-  const response = await axios.post(`${SERVICE_URL}/v1/me/history-booking`, params, {
-    headers: { Authorization: `Bearer ${decrypt(credential).token}` },
-  });
+  const response = await axios.post(
+    `${SERVICE_URL}/v1/${role}/history-booking`,
+    params,
+    {
+      headers: { Authorization: `Bearer ${decrypt(credential).token}` },
+    }
+  );
   return response ? response.data : null;
 };
