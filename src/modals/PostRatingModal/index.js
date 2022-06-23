ccbbe1be8bb9bc49eb23a0fc80769a679e8a12d2
Minh-Trung-SE <nguyenminhtrungse@gmail.com>
2022-06-23 07:13:50 +0700

    Temp change master branch

--- src/modals/PostRatingModal/index.js
+++ src/modals/PostRatingModal/index.js
@@ -5,7 +5,7 @@ import { EMPTY, TOAST_CONFIG } from '../../constants/default';
 import { postVote } from '../../services/me.service';
 
 const PostRatingModal = ({ bookingId, toogleShowModal, handleSubmitPostSuccess }) => {
-    
+
     const [score, setScore] = useState(0);
     const [comment, setComment] = useState(EMPTY);
     const [isSubmiting, setIsSubmiting] = useState(false)
@@ -20,53 +20,53 @@ const PostRatingModal = ({ bookingId, toogleShowModal, handleSubmitPostSuccess }
 
     const handlePostRating = async () => {
 
-        try{
+        try {
             setIsSubmiting(true);
             const data = JSON.stringify({
                 bookingId,
                 score,
                 comment: comment.trim().length > 0 ? comment.trim() : null
             });
-            
+
             await postVote(data);
             toast.success("Rating yard success!", TOAST_CONFIG);
             handleSubmitPostSuccess();
-        }catch(error){
+        } catch (error) {
             toast.error("Rating yard failed. Try again!", TOAST_CONFIG);
-        }finally{
+        } finally {
             setIsSubmiting(false)
         }
     }
 
     return (
         <>
-        <div className="custom-confirm">
-            <h4 className="w-min-content d-inline-block me-5">Rating</h4>
-            <Rating
-                onClick={handleOnChangeScoreRating}
-                ratingValue={score}
-                allowHalfIcon={true}
-            />
-            <p className="my-3">How do you feel about this yard?</p>
-            <textarea
-                className="w-100 mb-3"
-                name='comment'
-                value={comment}
-                onChange={handleOnChangeComment}
-                style={{ height: "100px", borderRadius: "5px", resize: "none" }}
-            />
-            <button
-                className="btn btn-primary me-3"
-                onClick={handlePostRating}
-                disabled={isSubmiting}
-            >
-                Confirm
-            </button>
-            <button className="btn btn-light" onClick={toogleShowModal}>
-                Cancel
-            </button>
-        </div>
-          <ToastContainer/>
+            <div className="custom-confirm">
+                <h4 className="w-min-content d-inline-block me-5">Rating</h4>
+                <Rating
+                    onClick={handleOnChangeScoreRating}
+                    ratingValue={score}
+                    allowHalfIcon={true}
+                />
+                <p className="my-3">How do you feel about this yard?</p>
+                <textarea
+                    className="w-100 mb-3"
+                    name='comment'
+                    value={comment}
+                    onChange={handleOnChangeComment}
+                    style={{ height: "100px", borderRadius: "5px", resize: "none" }}
+                />
+                <button
+                    className="btn btn-primary me-3"
+                    onClick={handlePostRating}
+                    disabled={isSubmiting}
+                >
+                    Confirm
+                </button>
+                <button className="btn btn-light" onClick={toogleShowModal}>
+                    Cancel
+                </button>
+            </div>
+            <ToastContainer />
         </>
 
     )
