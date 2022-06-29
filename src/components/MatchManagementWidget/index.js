24ff8b40c95fde67b6047a090070fcc2f0b1b9a1
ohlalata <nguyenbaotoan.8345@gmail.com>
2022-06-29 07:23:04 +0700

    create UI booking management first step

--- src/components/MatchManagementWidget/index.js
+++ src/components/MatchManagementWidget/index.js
@@ -1,7 +1,253 @@
 import "./style.scss";
 
 function MatchManagementWidget() {
-    return <div></div>;
+  return (
+    <div className="pt-4 w-100">
+      <div>
+        <div>
+          <h2 className="text-center">Booking Management</h2>
+        </div>
+        <div className="d-flex justify-content-center pt-3">
+          <div className="col-7">
+            <div className="border border-2 rounded">
+              <table class="table align-middle">
+                <thead>
+                  <tr>
+                    <th>Account</th>
+                    <th>Location</th>
+                    <th>Date/Time</th>
+                    <th>Slot</th>
+                    <th>Actions</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>
+                      <div class="d-flex align-items-center">
+                        <img
+                          src="https://sohanews.sohacdn.com/160588918557773824/2020/7/7/photo-1-1594135214879358461455.jpg"
+                          alt=""
+                          style={{ width: 45, height: 45 }}
+                          class="rounded-circle"
+                        />
+                        <div class="ms-2">
+                          <p>John Doe</p>
+                          <p class="text-muted">a@gmail.com</p>
+                        </div>
+                      </div>
+                    </td>
+                    <td>
+                      <p>Quận 1</p>
+                    </td>
+                    <td>
+                      <p>28/6/2022</p>
+                    </td>
+                    <td>
+                      <p>5 6</p>
+                    </td>
+                    <td>
+                      <button
+                        type="button"
+                        class="btn btn-link btn-rounded btn-sm fw-bold"
+                      >
+                        Cancel
+                      </button>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>
+                      <div class="d-flex align-items-center">
+                        <img
+                          src="https://sohanews.sohacdn.com/160588918557773824/2020/7/7/photo-1-1594135214879358461455.jpg"
+                          class="rounded-circle"
+                          alt=""
+                          style={{ width: 45, height: 45 }}
+                        />
+                        <div class="ms-2">
+                          <p>Alex Ray</p>
+                          <p class="text-muted">b@gmail.com</p>
+                        </div>
+                      </div>
+                    </td>
+                    <td>
+                      <p>Quận 1</p>
+                    </td>
+                    <td>
+                      <p>28/6/2022</p>
+                    </td>
+                    <td>
+                      <p>5 6</p>
+                    </td>
+                    <td>
+                      <button
+                        type="button"
+                        class="btn btn-link btn-rounded btn-sm fw-bold"
+                      >
+                        Cancel
+                      </button>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>
+                      <div class="d-flex align-items-center">
+                        <img
+                          src="https://sohanews.sohacdn.com/160588918557773824/2020/7/7/photo-1-1594135214879358461455.jpg"
+                          class="rounded-circle"
+                          alt=""
+                          style={{ width: 45, height: 45 }}
+                        />
+                        <div class="ms-2">
+                          <p>Kate</p>
+                          <p class="text-muted">c@gmail.com</p>
+                        </div>
+                      </div>
+                    </td>
+                    <td>
+                      <p>Quận 1</p>
+                    </td>
+                    <td>
+                      <p>28/6/2022</p>
+                    </td>
+                    <td>
+                      <p>5 6</p>
+                    </td>
+                    <td>
+                      <button
+                        type="button"
+                        class="btn btn-link btn-rounded btn-sm fw-bold"
+                      >
+                        Cancel
+                      </button>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+            <div className="pt-3">
+              <div className="border border-2 rounded">
+                <table class="table align-middle">
+                  <thead>
+                    <tr>
+                      <th>Account</th>
+                      <th>Location</th>
+                      <th>Date/Time</th>
+                      <th>Slot</th>
+                      <th>Actions</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td>
+                        <div class="d-flex align-items-center">
+                          <img
+                            src="https://sohanews.sohacdn.com/160588918557773824/2020/7/7/photo-1-1594135214879358461455.jpg"
+                            alt=""
+                            style={{ width: 45, height: 45 }}
+                            class="rounded-circle"
+                          />
+                          <div class="ms-2">
+                            <p>John Doe</p>
+                            <p class="text-muted">a@gmail.com</p>
+                          </div>
+                        </div>
+                      </td>
+                      <td>
+                        <p>Quận 1</p>
+                      </td>
+                      <td>
+                        <p>28/6/2022</p>
+                      </td>
+                      <td>
+                        <p>5 6</p>
+                      </td>
+                      <td>
+                        <button
+                          type="button"
+                          class="btn btn-link btn-rounded btn-sm fw-bold"
+                        >
+                          Cancel
+                        </button>
+                      </td>
+                    </tr>
+                    <tr>
+                      <td>
+                        <div class="d-flex align-items-center">
+                          <img
+                            src="https://sohanews.sohacdn.com/160588918557773824/2020/7/7/photo-1-1594135214879358461455.jpg"
+                            class="rounded-circle"
+                            alt=""
+                            style={{ width: 45, height: 45 }}
+                          />
+                          <div class="ms-2">
+                            <p>Alex Ray</p>
+                            <p class="text-muted">b@gmail.com</p>
+                          </div>
+                        </div>
+                      </td>
+                      <td>
+                        <p>Quận 1</p>
+                      </td>
+                      <td>
+                        <p>28/6/2022</p>
+                      </td>
+                      <td>
+                        <p>5 6</p>
+                      </td>
+                      <td>
+                        <button
+                          type="button"
+                          class="btn btn-link btn-rounded btn-sm fw-bold"
+                        >
+                          Cancel
+                        </button>
+                      </td>
+                    </tr>
+                    <tr>
+                      <td>
+                        <div class="d-flex align-items-center">
+                          <img
+                            src="https://sohanews.sohacdn.com/160588918557773824/2020/7/7/photo-1-1594135214879358461455.jpg"
+                            class="rounded-circle"
+                            alt=""
+                            style={{ width: 45, height: 45 }}
+                          />
+                          <div class="ms-2">
+                            <p>Kate</p>
+                            <p class="text-muted">c@gmail.com</p>
+                          </div>
+                        </div>
+                      </td>
+                      <td>
+                        <p>Quận 1</p>
+                      </td>
+                      <td>
+                        <p>28/6/2022</p>
+                      </td>
+                      <td>
+                        <p>5 6</p>
+                      </td>
+                      <td>
+                        <button
+                          type="button"
+                          class="btn btn-link btn-rounded btn-sm fw-bold"
+                        >
+                          Cancel
+                        </button>
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
+
+          <div className="col-5">
+            <h1>BBBBBBBB</h1>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default MatchManagementWidget;
