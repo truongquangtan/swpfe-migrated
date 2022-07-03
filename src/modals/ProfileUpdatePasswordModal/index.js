59b9b58f32bba76fdbb4cc746c24d8a9847068e7
ohlalata <nguyenbaotoan.8345@gmail.com>
2022-07-03 12:37:47 +0700

    Finish validate profile form and change password form

--- src/modals/ProfileUpdatePasswordModal/index.js
+++ src/modals/ProfileUpdatePasswordModal/index.js
@@ -1,75 +1,100 @@
 import { useFormik } from "formik";
-import { useCallback } from 'react';
-import { toast, ToastContainer } from 'react-toastify';
-import { TOAST_CONFIG } from '../../constants/default';
-import { encryptKey } from '../../helpers/crypto.helper';
-import { changePasswordRequest } from '../../services/me.service';
+import { useCallback } from "react";
+import { toast, ToastContainer } from "react-toastify";
+import { TOAST_CONFIG } from "../../constants/default";
+import { encryptKey } from "../../helpers/crypto.helper";
+import { changePasswordRequest } from "../../services/me.service";
 import { useNavigate } from "react-router-dom";
-
+import * as yup from "yup";
+import {
+  EMPTY,
+  REQUEST_PASSWORD,
+  REQUIRED_PASSWORD,
+} from "../../constants/default";
 const ProfileUpdatePasswordModal = ({ toggleModal }) => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const handleChangePasswordRequest = useCallback(async (values) => {
-        try {
-            const response = await changePasswordRequest(values);
-            localStorage.removeItem(encryptKey("credential"))
-            localStorage.setItem(encryptKey("temporaryToken"), response.token);
-            navigate("/auth/reset-password")
-        } catch (error) {
-            toast.error(error?.response?.data?.message, { ...TOAST_CONFIG, containerId: "update-password-modal" })
-        }
-    }, []);
+  const handleChangePasswordRequest = useCallback(async (values) => {
+    try {
+      const response = await changePasswordRequest(values);
+      localStorage.removeItem(encryptKey("credential"));
+      localStorage.setItem(encryptKey("temporaryToken"), response.token);
+      navigate("/auth/reset-password");
+    } catch (error) {
+      toast.error(error?.response?.data?.message, {
+        ...TOAST_CONFIG,
+        containerId: "update-password-modal",
+      });
+    }
+  }, []);
 
-    const formik = useFormik({
-        initialValues: { password: "" },
-        validateOnMount: true,
-        validateOnChange: true,
-        onSubmit: async (values) => {
-            formik.setSubmitting(true)
-            await handleChangePasswordRequest(values)
-            formik.setSubmitting(false)
-        }
-    })
+  const validation = yup.object({
+    password: yup
+      .string(REQUEST_PASSWORD)
+      .required(REQUIRED_PASSWORD)
+      .min(8, "Password should be of minimum 8 characters length"),
+  });
 
-    return (
+  const formik = useFormik({
+    initialValues: { password: EMPTY },
+    validateOnMount: true,
+    validateOnChange: true,
+    validationSchema: validation,
+    onSubmit: async (values) => {
+      formik.setSubmitting(true);
+      await handleChangePasswordRequest(values);
+      formik.setSubmitting(false);
+    },
+  });
 
-        <div
-            className="rounded border border-3 custom-confirm"
-            style={{ width: 500 }}
-        >
-            <form className="my-3" onSubmit={formik.handleSubmit}>
-                <div className="row p-1">
-                    <label
-                        htmlFor="old-password"
-                        className="text-start"
-                        style={{ paddingLeft: 0 }}
-                    >
-                        Your Password
-                    </label>
-                    <span className="col-1 lh-44 signup__icon-wrapper">
-                        <i className="fas fa-user-lock"></i>
-                    </span>
-                    <input
-                        className="col-11 outline-none p-2 signup__input-border"
-                        type="password"
-                        placeholder="Enter your password..."
-                        name="password"
-                        value={formik.values.password}
-                        onChange={formik.handleChange}
-                    />
-                </div>
-                <div className="mt-3 d-flex justify-content-center">
-                    <button disabled={!formik.isValid | formik.isSubmitting} type="submit" className="btn btn-primary px-4">
-                        Confirm
-                    </button>
-                    <div className="btn btn-light mx-4 cancel" onClick={toggleModal}>
-                        Cancel
-                    </div>
-                </div>
-            </form>
-            <ToastContainer containerId="update-password-modal"/>
+  return (
+    <div
+      className="rounded border border-3 custom-confirm"
+      style={{ width: 500 }}
+    >
+      <form className="my-3" onSubmit={formik.handleSubmit}>
+        <div className="row p-1">
+          <label
+            htmlFor="old-password"
+            className="text-start"
+            style={{ paddingLeft: 0 }}
+          >
+            Your Password
+          </label>
+          <span className="col-1 lh-44 signup__icon-wrapper">
+            <i className="fas fa-user-lock"></i>
+          </span>
+          <input
+            className="col-11 outline-none p-2 signup__input-border"
+            type="password"
+            placeholder="Enter your password..."
+            name="password"
+            value={formik.values.password}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+          />
+          <span className="signup__filed--error">
+            {formik.touched.password && formik.errors.password
+              ? formik.errors.password
+              : EMPTY}{" "}
+          </span>
         </div>
-    )
-}
+        <div className="mt-3 d-flex justify-content-center">
+          <button
+            disabled={!formik.isValid | formik.isSubmitting}
+            type="submit"
+            className="btn btn-primary px-4"
+          >
+            Confirm
+          </button>
+          <div className="btn btn-light mx-4 cancel" onClick={toggleModal}>
+            Cancel
+          </div>
+        </div>
+      </form>
+      <ToastContainer containerId="update-password-modal" />
+    </div>
+  );
+};
 
-export default ProfileUpdatePasswordModal
+export default ProfileUpdatePasswordModal;
