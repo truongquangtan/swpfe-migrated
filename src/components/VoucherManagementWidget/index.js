9fcf94c2b346ce2578e38d283816c11654bec37e
Giang <kaylin.pham@contemi.com>
2022-07-14 10:57:08 +0700

    restyle

--- src/components/VoucherManagementWidget/index.js
+++ src/components/VoucherManagementWidget/index.js
@@ -181,7 +181,7 @@ function VoucherManagementWidget() {
   };
 
   return (
-    <div className="pt-5 mt-5 w-100">
+    <div className="pt-4 mt-5 w-100">
       <h4 className="mb-4 d-inline-block">
         <img src={coupon} alt="Yard" className="width-60 pe-3" />
         Voucher Management
