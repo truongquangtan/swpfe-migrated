1fa848fd7498813d39e9c6fe934ff05081b3fe16
Giang <kaylin.pham@contemi.com>
2023-01-12 23:18:59 +0700

    remove strict mode

--- src/index.js
+++ src/index.js
@@ -7,8 +7,4 @@ import "./assets/fonts/fontawesome/css/all.css";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<App />);
